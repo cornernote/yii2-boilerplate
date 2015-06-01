de4e6ec83441053acdc991e9934455ed4e9c85fe
Brett O'Donnell <cornernote@gmail.com>
2015-06-01 10:29:35 +0930

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 Yii::setAlias('@app', dirname(dirname(__DIR__)));
 Yii::setAlias('@web', dirname(__DIR__) . '/web');
+Yii::setAlias('@workflowDefinitionNamespace', 'app\\models\\workflow');
+\Yii::$container->set('yii\bootstrap\ActiveForm', [
+    'errorSummaryCssClass' => 'alert alert-danger error-summary',
+]);
