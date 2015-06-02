70c17888b771bf882a35248d1362b184414bcf1f
Brett O'Donnell <cornernote@gmail.com>
2015-06-02 18:39:30 +0930

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -1,11 +1,11 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'Delivery';
+$this->title = Yii::t('app', 'Home');
 ?>
 <div class="site-index ">
 
     <h1><?= getenv('APP_NAME') ?></h1>
 
-    <h2 class="lead">Up and running.</h2>
+    <h2 class="lead"><?= Yii::t('app', 'Up and running.') ?></h2>
 
 </div>
