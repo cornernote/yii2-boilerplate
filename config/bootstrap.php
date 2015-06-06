6555f3b3479cfde1f1ed181d70b7b8fdba1e6728
Brett O'Donnell <cornernote@gmail.com>
2015-06-06 15:54:45 +0930

    Update bootstrap.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,15 +3,15 @@ Yii::setAlias('@app', dirname(dirname(__DIR__)));
 Yii::setAlias('@web', dirname(__DIR__) . '/web');
 Yii::setAlias('@workflowDefinitionNamespace', 'app\\models\\workflow');
 
-\Yii::$container->set('yii\bootstrap\ActiveForm', [
+Yii::$container->set('yii\bootstrap\ActiveForm', [
     'errorSummaryCssClass' => 'alert alert-danger error-summary',
 ]);
 
-\Yii::$container->set('yii\widgets\LinkPager', [
+Yii::$container->set('yii\widgets\LinkPager', [
     'firstPageLabel' => Yii::t('app', 'First'),
     'lastPageLabel' => Yii::t('app', 'Last')
 ]);
 
-\Yii::$container->set('yii\bootstrap\Alert', [
+Yii::$container->set('yii\bootstrap\Alert', [
     'closeButton' => ['label' => '<i class="fa fa-times"></i>'],
 ]);
