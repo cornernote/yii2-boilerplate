d3f608768dbcef19243360c485eb679822d21fce
Brett O'Donnell <cornernote@gmail.com>
2016-05-20 12:10:16 +0930

    Update main.php

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -1,9 +1,11 @@
 <?php
 use app\assets\AppAsset;
+use app\widgets\PreventDoubleSubmit;
 
 //$viewSidebar = '@app/views/layouts/_sidebar';
 
 AppAsset::register($this);
+PreventDoubleSubmit::widget();
 
 $regex = '|(\\' . DIRECTORY_SEPARATOR . '[^\\' . DIRECTORY_SEPARATOR . ']*\\' . DIRECTORY_SEPARATOR . '[^\\' . DIRECTORY_SEPARATOR . ']*\.php)$|';
 preg_match($regex, __FILE__, $matches);
